f45dd7ee0868755358d251326652391604f2a852
Ricardo Alegria <ricardo_alegria@siman.com>
2025-03-06 14:50:19 -0600

    giftMessage component added to audio

--- src/components/Audio/index.tsx
+++ src/components/Audio/index.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Button } from "antd";
+import { Alert, Button } from "antd";
 import { useRef, useState } from "react";
+import { GiftFilled } from "@ant-design/icons";
 
 interface AudioProps {
   children?: React.ReactNode;
@@ -18,10 +19,26 @@ const BackgroundAudio = ({ children }: AudioProps) => {
     }
   };
 
+  const GiftMessage = () => (
+    <Alert
+      message="Ten en cuenta"
+      description={
+        <>
+          Agradecemos tus muestras de cariño en <b>regalo de sobre</b>
+        </>
+      }
+      className="mt-[40px]"
+      type="success"
+      icon={<GiftFilled />}
+      showIcon
+    />
+  );
+
   return (
     <>
       {!playClicked && (
-        <div className="flex flex-col gap-[40px] items-center p-[90px_24px]">
+        <div className="flex flex-col gap-[40px] items-center p-[40px_24px]">
+          <GiftMessage />
           <h1 className="text-white text-center text-[32px] font-[family-name:var(--font-hahmlet)]">
             ¿Listo para celebrar conmigo?
           </h1>
@@ -34,7 +51,12 @@ const BackgroundAudio = ({ children }: AudioProps) => {
           </Button>
         </div>
       )}
-      {playClicked && <>{children}</>}
+      {playClicked && (
+        <>
+          <GiftMessage />
+          {children}
+        </>
+      )}
       <audio ref={audioRef} src={"/sounds/brave.mp3"} controls={false} loop />
     </>
   );
